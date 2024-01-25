d3146c9db573db6e0edbcebdad8902f146d5f5ab
Vinay Kumar <100464411+Vinay7k7@users.noreply.github.com>
2024-01-25 13:40:11 +0530

    Add files via upload

--- mod.py
+++ mod.py
@@ -3,6 +3,7 @@ import time
 from kafka import KafkaProducer
 import requests
 from concurrent.futures import ThreadPoolExecutor
+import threading
 
 # Kafka producer configuration
 bootstrap_servers = 'localhost:9092'  # Replace with your Kafka bootstrap servers
@@ -28,26 +29,37 @@ def get_weather_data(city):
         data = response.json()
         if response.status_code == 200:
             producer.send(kafka_topic, value=data)
-            print("Weather data for {} pushed to Kafka: {}".format(city,data))
+            print("Weather data for {} pushed to Kafka: {}".format(city, data))
         else:
-            print("Failed to fetch data for {}. Status code: {}".format(city,response.status_code))
+            print("Failed to fetch data for {}. Status code: {}".format(city, response.status_code))
     except requests.RequestException as e:
         print("Request Exception: {}".format(e))
 
 def fetch_data_and_push(city):
-    try:
-        get_weather_data(city)
-    except Exception as e:
-        print("Error for city {}: {}".format(city,e))
+    while not exit_flag:
+        try:
+            get_weather_data(city)
+            time.sleep(10)  # Sleep for 10 seconds before fetching data for the same city again
+        except Exception as e:
+            print("Error for city {}: {}".format(city, e))
 
 def main():
-    with ThreadPoolExecutor(max_workers=len(CITIES)) as executor:
-        try:
-            executor.map(fetch_data_and_push, CITIES)
-        except KeyboardInterrupt:
-            print("Script interrupted by user.")
-            global exit_flag
-            exit_flag = True  # Set the exit flag to True
+    threads = []
+
+    # Create and start a thread for each city
+    for city in CITIES:
+        thread = threading.Thread(target=fetch_data_and_push, args=(city,))
+        thread.start()
+        threads.append(thread)
+
+    try:
+        # Keep the main thread alive while the city threads are running
+        for thread in threads:
+            thread.join()
+    except KeyboardInterrupt:
+        print("Script interrupted by user.")
+        global exit_flag
+        exit_flag = True  # Set the exit flag to True
 
 if __name__ == "__main__":
     main()
